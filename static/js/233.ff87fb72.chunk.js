bbcd0531637b66370556995241f282197b8becc8
ArtomkIvanher <161267462+ArtomkIvanher@users.noreply.github.com>
2024-07-08 22:18:36 +0300

    Updates

--- static/js/233.ff87fb72.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkunesco=self.webpackChunkunesco||[]).push([[233],{233:e=>{e.exports=JSON.parse('[{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""},{"year1":"","title1":"","text1":"","year2":"","title2":"","text2":""}]')}}]);
